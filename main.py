7d63ac25550f7edf41b6cf21b4ee8aa2e66400cb
martywebdev <martywebdevelopment@gmail.com>
2025-09-16 21:19:49 +0800

    add menu bar

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ from PyQt6.QtWidgets import (
     QWidget,
     QGridLayout,
     QLineEdit,
-    QMainWindow)
+    QMainWindow
+)
+
+from PyQt6.QtGui import QAction
 
 
 class MainWindow(QMainWindow):
@@ -14,11 +17,15 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Studend Management System")
-        
+
         file_menu_item = self.menuBar().addMenu("&File")
         help_menu_item = self.menuBar().addMenu("&Help")
-        
-        
+
+        add_student_action = QAction("Add Student", self)
+        file_menu_item.addAction(add_student_action)
+
+        about_action = QAction('About', self)
+        help_menu_item.addAction(about_action)
 
 
 app = QApplication(sys.argv)
